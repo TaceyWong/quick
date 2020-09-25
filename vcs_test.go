9a14448b4be24e1ca5f4df7036c130208beeb1f9
TaceyWong <xinyong.wang@xtalpi.com>
2020-09-25 16:43:48 +0800

    add:vcs-test

---
+++ vcs_test.go
@@ -0,0 +1,9 @@
+/*
+ * @Description: 填写描
+ * @Author: WangXinyong/TaceyWong
+ * @Date: 2020-09-25 10:44:55
+ * @LastEditors: WangXinyong/TaceyWong
+ * @LastEditTime: 2020-09-25 10:45:05
+ * @FilePath: /quick/cvs_test.go
+ */
+pacakge quick
