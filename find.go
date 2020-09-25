56f3c8ae16993107e9a1e88559599cc3c5a64edd
TaceyWong <xinyong.wang@xtalpi.com>
2020-09-25 16:39:59 +0800

    ADD:find template function

--- find.go
+++ find.go
@@ -8,11 +8,32 @@
 
 package quick
 
+
+
 // FindTemplate Determine which child directory of `repo_dir` is the project template.
 // 
 // :param repoDir: Local directory of newly cloned repo.
 // :returns projectTemplate: Relative path to project template.
 func FindTemplate(repoDir string)string{
-	return "~/.quick"
+	log.Debug(fmt.SPrintf("Searching %s for the project template.", repoDir))
+	repo_dir_contents, err := ioutil.ReadDir("./")
+    if err != nil {
+        log.Fatal(err)
+	}
+	projectTemplate := ""
+    for _, item := range repo_dir_contents {
+		name := item.Name()
+		if strings.Contains(name,"quick") && strings.Contains(name,"{{") && 
+		   strings.Contains(name,"}}"){
+			projectTemplate = name
+			break
+		}
+	}
+	if projectTemplate != nil{
+		projectTemplate = repoDir+projectTemplate
+		log.Debug(fmt.SPrintf("The project template appears to be %s", projectTemplate))
+		return projectTemplate
+	}
+	return "~/.quick" // NonTemplatedInputDirException
 }
 
