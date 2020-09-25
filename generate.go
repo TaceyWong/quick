809ecc02c6a774dbac6987498ca361768574a25c
TaceyWong <xinyong.wang@xtalpi.com>
2020-09-25 17:06:00 +0800

    update pongo2 import

--- generate.go
+++ generate.go
@@ -6,4 +6,21 @@
 * @Last Modified time: 2020-09-23 17:48:32
 */
 package quick
+import (
+	"github.com/flosch/pongo2/v4"
+)
 
+func Example(){
+	// Compile the template first (i. e. creating the AST)
+tpl, err := pongo2.FromString("Hello {{ name|capfirst }}!")
+if err != nil {
+    panic(err)
+}
+// Now you can render the template with the given
+// pongo2.Context how often you want to.
+out, err := tpl.Execute(pongo2.Context{"name": "florian"})
+if err != nil {
+    panic(err)
+}
+fmt.Println(out)
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/AlecAivazis/survey/v2 v2.1.1
+	github.com/flosch/pongo2/v4 v4.0.0
 	github.com/go-git/go-git/v5 v5.1.0
 	github.com/micro/go-micro/v2 v2.9.1 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
