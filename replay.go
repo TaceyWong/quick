41f8dac2c4f65ca941ea751ac77c167cefddd08e
TaceyWong <xinyong.wang@xtalpi.com>
2020-09-30 11:41:19 +0800

    add package name

--- replay.go
+++ replay.go
@@ -1,7 +1,24 @@
 /*
+* Replay Quick action
 * @Author: TaceyWong
 * @Date:   2020-09-23 16:59:26
 * @Last Modified by:   TaceyWong
-* @Last Modified time: 2020-09-23 17:14:08
+* @Last Modified time: 2020-09-27 14:50:38
 */
 package quick
+
+
+// GetFileName Get the name of file.
+func GetFileName(replayDir,templateName string){
+
+}
+
+// Dump Write json data to file.
+func Dump(replayDir,templateName,context string){
+
+}
+
+// Load Read json data from file.
+func Load(replayDir,templateName string){
+
+}
